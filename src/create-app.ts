2b82c955a14eb81c1ad5c4c9782dfd4868a251b2
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-09-08 17:34:42 -0400

    proto

--- src/create-app.ts
+++ src/create-app.ts
@@ -7,6 +7,8 @@ import { Starter } from './starters';
 import { unZipBuffer } from './unzip';
 import { npm, onlyUnix, printDuration, setTmpDirectory, terminalPrompt } from './utils';
 import { replaceInFile } from 'replace-in-file';
+import { execSync } from 'child_process';
+import { getPkgVersion } from './version';
 
 const starterCache = new Map<Starter, Promise<undefined | ((name: string) => Promise<void>)>>();
 
@@ -34,9 +36,23 @@ export async function createApp(starter: Starter, projectName: string, autoRun: 
   loading.stop(true);
 
   const time = printDuration(Date.now() - startT);
+  let hasErr = false;
+  if (!changeDir(projectName)) {
+    hasErr = hasErr || true; // I know, weird
+  }
+
+  if (!initGit()) {
+    hasErr = hasErr || true;
+  }
+
+  // TODO() we init git, do we clean up if we failed?
+
+  if (!commitGit()) {
+    hasErr = hasErr || true;
+  }
+
   console.log(`${green('✔')} ${bold('All setup')} ${onlyUnix('🎉')} ${dim(time)}
 
-  ${dim('We suggest that you begin by typing:')}
 
   ${dim(terminalPrompt())} ${green('cd')} ${projectName}
   ${dim(terminalPrompt())} ${green('npm install')}
@@ -111,4 +127,49 @@ async function prepare(starter: Starter) {
 
 function validateProjectName(projectName: string) {
   return !/[^a-zA-Z0-9-]/.test(projectName);
+}
+
+export function changeDir(moveTo: string): boolean {
+  console.log(`in ${process.cwd()}`);
+  let wasSuccess = false;
+  try {
+    process.chdir(moveTo);
+    wasSuccess = true;
+  } catch (err: unknown) {
+    console.error(err);
+  }
+  return wasSuccess;
+}
+export function initGit(): boolean {
+  let wasSuccess = false;
+  try {
+    // if `git` is not on the user's path, this will return a non-zero exit code
+    // also returns a non-zero exit code if it times out
+    execSync('git --version', { stdio: 'ignore' });
+
+    // TODO(NOW): we may be in a subtree, which may not be desirable?
+
+    // git must exist on the path, initialize a repo
+    // init can fail for reasons like a malformed git config, permissions, etc.
+    // we assume that git will continue to allow `git init` in a pre-existing repo
+    execSync('git init', { stdio: 'ignore' });
+    wasSuccess = true;
+  } catch (err: unknown) {
+    console.error(err);
+  }
+  return wasSuccess;
+}
+
+export function commitGit(): boolean {
+  let wasSuccess = false;
+  try {
+    // add all files (including dotfiles)
+    execSync('git add -A', { stdio: 'ignore' });
+    // commit them
+    execSync(`git commit -m "init with create-stencil ${getPkgVersion()}"`, { stdio: 'ignore' });
+    wasSuccess = true;
+  } catch (err: unknown) {
+    console.error(err);
+  }
+  return wasSuccess;
 }
