52c1d3bf61a196742608e31da53087d2bf706c5e
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-09-09 12:08:11 -0400

    some tests

--- src/git.spec.ts
+++ src/git.spec.ts
@@ -1,7 +1,46 @@
+import * as cp from 'child_process';
+import { inExistingGitTree } from './git';
+
+// TODO(NOW): Handle console statements
 describe('git', () => {
+  let execSyncSpy: jest.SpyInstance<ReturnType<typeof cp.execSync>, Parameters<typeof cp.execSync>>;
+  let consoleSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    execSyncSpy = jest.spyOn(cp, 'execSync');
+  });
+
+  afterEach(() => {
+    execSyncSpy.mockRestore();
+  });
+
   describe('inExistingGitTree', () => {
-    it('todo', () => {
-      fail('todo!');
+    beforeEach(() => {
+      execSyncSpy.mockImplementation((cmd: string, _options: cp.ExecOptions | undefined) => {
+        switch (cmd) {
+          case 'git rev-parse --is-inside-work-tree':
+            return Buffer.alloc(0);
+          default:
+            throw new Error(`unmocked command ${cmd}`);
+        }
+      });
+    });
+
+    describe('is in existing git repo', () => {
+      it('returns status of true', () => {
+        const isInExistingGitTree = inExistingGitTree();
+        expect(isInExistingGitTree).toBe(true);
+      });
+    });
+
+    describe('is not in existing git repo', () => {
+      it('returns status of false', () => {
+        execSyncSpy.mockImplementation(() => {
+          throw new Error('fatal: not a git repository (or any of the parent directories): .git');
+        });
+        const isInExistingGitTree = inExistingGitTree();
+        expect(isInExistingGitTree).toBe(false);
+      });
     });
   });
 
